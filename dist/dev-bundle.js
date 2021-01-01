b970d54c6b90ee190e1c5f3fd08bdc12683e5e41
davas93 <vasin.da93@gmail.com>
2021-01-01 17:00:22 +0300

    audioControl completed

--- dist/dev-bundle.js
+++ dist/dev-bundle.js
@@ -49,7 +49,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mod
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\nvar audioControl = function audioControl() {};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (audioControl);\n\n//# sourceURL=webpack://3DGLO/./src/modules/audioControl.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\nvar audioControl = function audioControl() {\n  var music = document.querySelector('.music'),\n      range = document.querySelector('.range'),\n      volume = document.querySelector('.volume-icon'),\n      volumeStatus = volume.querySelector('use');\n  music.volume = range.value / 100;\n  range.addEventListener('input', function () {\n    music.volume = range.value / 100;\n\n    if (range.value == 0) {\n      volumeStatus.setAttribute('xlink:href', '/sprite.svg#volume-off');\n    } else {\n      volumeStatus.setAttribute('xlink:href', '/sprite.svg#volume');\n    }\n  });\n  volume.addEventListener('click', function () {\n    if (music.volume !== 0) {\n      music.volume = 0;\n      range.value = 0;\n      volumeStatus.setAttribute('xlink:href', '/sprite.svg#volume-off');\n    } else {\n      volumeStatus.setAttribute('xlink:href', '/sprite.svg#volume');\n      range.value = 10;\n      music.volume = range.value / 100;\n    }\n  });\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (audioControl);\n\n//# sourceURL=webpack://3DGLO/./src/modules/audioControl.js?");
 
 /***/ }),
 
@@ -455,7 +455,7 @@ eval("var map = {\n\t\"./log\": \"./node_modules/webpack/hot/log.js\"\n};\n\n\nf
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => "ab0a2940793c0164191b"
+/******/ 		__webpack_require__.h = () => "4a9977204285305cbd7c"
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/modules/audioControl.js
+++ src/modules/audioControl.js
@@ -1,3 +1,31 @@
-const audioControl = () => {};
+const audioControl = () => {
+	const music = document.querySelector('.music'),
+		range = document.querySelector('.range'),
+		volume = document.querySelector('.volume-icon'),
+		volumeStatus = volume.querySelector('use');
+
+	music.volume = range.value / 100;
+
+	range.addEventListener('input', () => {
+		music.volume = range.value / 100;
+		if (range.value == 0) {
+			volumeStatus.setAttribute('xlink:href', '/sprite.svg#volume-off');
+		} else {
+			volumeStatus.setAttribute('xlink:href', '/sprite.svg#volume');
+		}
+	});
+
+	volume.addEventListener('click', () => {
+		if (music.volume !== 0) {
+			music.volume = 0;
+			range.value = 0;
+			volumeStatus.setAttribute('xlink:href', '/sprite.svg#volume-off');
+		} else {
+			volumeStatus.setAttribute('xlink:href', '/sprite.svg#volume');
+			range.value = 10;
+			music.volume = range.value / 100;
+		}
+	});
+};
 
 export default audioControl;
