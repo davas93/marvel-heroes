76ccf3d1540e8a2bd19974de346e55a848712c27
davas93 <vasin.da93@gmail.com>
2021-01-02 17:06:03 +0300

    output getData

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -10,32 +10,23 @@ const getData = () => {
 		})
 		.then((data) => {
 			data.forEach((item) => {
+				let info = '';
+
+				Object.keys(item).forEach((key) => {
+					console.log(item);
+
+					let value = item[key];
+					if (value && key !== 'photo') {
+						info += `<div class="hero-data">
+							<span>${key}:</span>
+							<span>${value}</span>
+						</div>`;
+					}
+				});
+
 				const heroCard = `<div class="hero-card">
 						<img src="https://raw.githubusercontent.com/Quper24/dbHeroes/master/${item.photo}" alt="${item.name}" class="hero-photo" />
-						<div class="hero-info">
-							<div class="hero-label">
-								<span class="key">Name:</span>
-								<span class="key">Real name:</span>
-								<span class="key">Species:</span>
-								<span class="key">Citizenship:</span>
-								<span class="key">Gender:</span>
-								<span class="key">Birthday:</span>
-                                <span class="key">Actors:</span>
-                                <span class="key">Status:</span>
-								<span class="key">Movies:</span>
-							</div>
-							<div class="hero-descr">
-								<span class="sense" id="name">${item.name}</span>
-								<span class="sense" id="real-name">${item.realName}</span>
-								<span class="sense" id="species">${item.species}</span>
-								<span class="sense" id="citizenship">${item.citizenship}</span>
-								<span class="sense" id="gender">${item.gender}</span>
-								<span class="sense" id="birthday">${item.birthDay}</span>
-                                <span class="sense" id="actors">${item.actors}</span>
-                                <span class="sense" id="status">${item.status}</span>
-								<span class="sense" id="movies">${item.movies}</span>
-							</div>
-						</div>
+						<div class="hero-info">${info}</div>
                     </div>`;
 
 				cards.insertAdjacentHTML('beforeend', heroCard);
