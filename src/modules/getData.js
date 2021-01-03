a15efd4982a840721c6be78ae6a9efb5536a369b
davas93 <vasin.da93@gmail.com>
2021-01-03 11:39:03 +0300

    add dropdown item from data

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -13,8 +13,6 @@ const getData = () => {
 				let info = '';
 
 				Object.keys(item).forEach((key) => {
-					console.log(item);
-
 					let value = item[key];
 					if (value && key !== 'photo') {
 						info += `<div class="hero-data">
@@ -31,6 +29,60 @@ const getData = () => {
 
 				cards.insertAdjacentHTML('beforeend', heroCard);
 			});
+
+			const createFilterByMovies = () => {
+				const film = document.getElementById('film');
+
+				const result = data.map(({ movies }) => movies);
+				const merged = [].concat.apply([], result);
+				const unique = Array.from(new Set(merged)).sort();
+
+				unique.forEach((item) => {
+					if (item !== undefined) {
+						film.insertAdjacentHTML(
+							'beforeend',
+							`<li class="dropdown-item">${item}</li>`
+						);
+					}
+				});
+			};
+			createFilterByMovies();
+
+			const createFilterByGender = () => {
+				const gender = document.getElementById('gender');
+
+				const result = data.map(({ gender }) => gender);
+				const merged = [].concat.apply([], result);
+				const unique = Array.from(new Set(merged)).sort();
+
+				unique.forEach((item) => {
+					if (item !== undefined) {
+						gender.insertAdjacentHTML(
+							'beforeend',
+							`<li class="dropdown-item">${item}</li>`
+						);
+					}
+				});
+			};
+			createFilterByGender();
+
+			const createFilterBySpecies = () => {
+				const species = document.getElementById('species');
+
+				const result = data.map(({ species }) => species);
+				const merged = [].concat.apply([], result);
+				const unique = Array.from(new Set(merged)).sort();
+
+				unique.forEach((item) => {
+					if (item !== undefined) {
+						species.insertAdjacentHTML(
+							'beforeend',
+							`<li class="dropdown-item">${item}</li>`
+						);
+					}
+				});
+			};
+			createFilterBySpecies();
 		})
 		.catch((error) => {
 			console.error(error);
