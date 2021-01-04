5790795c586de52b6ba1abe7448ef33b248d4add
davas93 <vasin.da93@gmail.com>
2021-01-04 20:34:28 +0300

    fixed dropdown

--- src/modules/filter.js
+++ src/modules/filter.js
@@ -4,6 +4,14 @@ const filter = () => {
 	select.forEach((item) => {
 		item.addEventListener('click', (event) => {
 			let target = event.target;
+			let sibling = target.nextElementSibling;
+			let id = sibling ? sibling.id : null;
+
+			document.querySelectorAll('.dropdown').forEach((item) => {
+				if (item.id !== id) {
+					item.classList.remove('dropdown-active');
+				}
+			});
 
 			const drop = item.querySelector('.dropdown'),
 				input = item.querySelector('input');
@@ -18,7 +26,10 @@ const filter = () => {
 	});
 
 	document.addEventListener('click', (event) => {
-		if (!event.target.closest('.select-block')) {
+		if (
+			!event.target.closest('.select-block') ||
+			event.target.classList.contains('dropdown-item')
+		) {
 			const drop = document.querySelectorAll('.dropdown');
 
 			drop.forEach((item) => {
