990f86f52bcf0b67c8861e0eb6e4edfecd7f6a22
davas93 <vasin.da93@gmail.com>
2021-01-06 16:53:22 +0300

    filter by

--- src/modules/filter.js
+++ src/modules/filter.js
@@ -15,13 +15,52 @@ const filter = () => {
 
 			const drop = item.querySelector('.dropdown'),
 				input = item.querySelector('input');
+
 			drop.classList.toggle('dropdown-active');
 
 			if (target.matches('li')) {
 				input.value =
 					target.textContent.charAt(0).toUpperCase() +
 					target.textContent.slice(1);
+
+				input.dispatchEvent(
+					new CustomEvent('change', { detail: drop.id })
+				);
 			}
+		});
+
+		item.querySelector('input').addEventListener('change', (e) => {
+			document.querySelectorAll('.hero-card').forEach((card, i) => {
+				if ('gender' === e.detail) {
+					window.filterGender = e.target.value.toLowerCase();
+				}
+				if ('species' === e.detail) {
+					window.filterSpecies = e.target.value.toLowerCase();
+				}
+				if ('film' === e.detail) {
+					window.filterFilm = e.target.value;
+				}
+
+				if (
+					(window.cards[i].gender === window.filterGender ||
+						window.filterGender === 'all' ||
+						!window.filterGender) &&
+					((window.cards[i].species &&
+						window.cards[i].species.toLowerCase() ===
+							window.filterSpecies) ||
+						window.filterSpecies === 'all' ||
+						!window.filterSpecies) &&
+					((window.cards[i].movies &&
+						window.cards[i].movies.indexOf(window.filterFilm) !==
+							-1) ||
+						window.filterFilm === 'All' ||
+						!window.filterFilm)
+				) {
+					card.style.display = 'block';
+				} else {
+					card.style.display = 'none';
+				}
+			});
 		});
 	});
 
